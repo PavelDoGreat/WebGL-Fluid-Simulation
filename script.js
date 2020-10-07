a379196fd60448eebf927cd8d3460396c783a4d8
giacomotg <giacomotg@gmail.com>
2020-10-07 16:57:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ let config = {
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
     BACK_COLOR: { r: 0, g: 0, b: 0 },
-    TRANSPARENT: false,
+    TRANSPARENT: true,
     BLOOM: true,
     BLOOM_ITERATIONS: 8,
     BLOOM_RESOLUTION: 256,
